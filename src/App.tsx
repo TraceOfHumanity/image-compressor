73eaf1398324f07742f3276311933d43d61e765b
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-20 16:29:17 +0200

    image compressed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import {useState} from "react";
 import {FileInput} from "./components/fileInput";
 
 function App() {
+  const [file, setFile] = useState();
+  // console.log(file);
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setFile(file);
+    }
+  };
   return (
     <div className="container h-screen max-h-screen px-4 py-8 md:px-8 md:py-16">
       <h1 className="text-2xl font-bold text-center">Image compressor</h1>
-      <FileInput />
+      <input type="file" onChange={handleFileChange} accept="image/*" />
+      <FileInput file={file} />
     </div>
   );
 }
--- src/components/fileInput/index.tsx
+++ src/components/fileInput/index.tsx
@@ -1,17 +1,50 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import Resizer from "react-image-file-resizer";
+import prettyBytes from "pretty-bytes";
 
-export const FileInput = () => {
-  const [imageAssets, setImageAssets] = useState([]);
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const files = Array.from(event.target.files || []);
-    setImageAssets(files);
+export const FileInput = ({file}: {file: File}) => {
+  const [newImage, setNewImage] = useState();
+  
+  const handleImage = (image: {url: string; size: number; type: string}) => {
+    setNewImage(image);
   };
 
-  console.log(imageAssets);
+  useEffect(() => {
+    if (file) {
+      console.log("File received", file);
+      try {
+        Resizer.imageFileResizer(
+          file,
+          1500,
+          1500,
+          "JPEG",
+          90,
+          0,
+          (blob) => {
+            handleImage({
+              url: URL.createObjectURL(blob),
+              size: blob.size,
+              type: blob.type,
+            });
+          },
+          "blob",
+          400,
+          400
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, [file]);
+
+  if (!newImage) {
+    return null;
+  }
+
   return (
-    <div className="shadow-lg rounded-md p-4 backdrop-blur-sm">
-      <input type="file" multiple onChange={handleInputChange} />
-    </div>
+    <>
+      <figure>{`${prettyBytes(newImage.size)} ${newImage.type}`}</figure>
+      <img src={newImage.url} alt="" style={{maxWidth: "100%"}} />
+    </>
   );
 };
