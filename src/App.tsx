dcef796657930f680e630e8223c52e4970792452
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-21 21:40:19 +0200

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import {CompressedImage} from "./components/CompressedImage";
+import {FaDownload} from "react-icons/fa";
 
 function App() {
   const [files, setFiles] = useState<File[]>([]);
@@ -26,10 +27,10 @@ function App() {
   };
 
   return (
-    <div className="container h-screen max-h-screen px-4 py-8 md:px-8 md:py-16">
+    <div className="container h-screen max-h-screen px-4 py-8 md:px-8 md:py-16 flex flex-col gap-4">
       <h1 className="text-2xl font-bold text-center">Image compressor</h1>
 
-      <div className="border-2 w-full h-40 border-dashed border-mainText rounded-md bg-mainBg">
+      <div className="my-auto border-2 w-full min-h-40 border-dashed border-mainText rounded-md bg-mainBg relative">
         <input
           className="w-full h-full opacity-0"
           type="file"
@@ -37,13 +38,21 @@ function App() {
           accept="image/*"
           multiple
         />
+        <p className="text-mainText text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          Drag and drop or click to select images
+        </p>
       </div>
 
       {compressedImages.length > 0 && (
-        <button onClick={downloadAll}>Download all</button>
+        <button
+          className="flex items-center gap-2 w-fit ml-auto"
+          onClick={downloadAll}
+        >
+          Download all <FaDownload />
+        </button>
       )}
 
-      <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 pb-5 md:pb-16" >
         {files.map((file) => (
           <CompressedImage
             key={file.name}
